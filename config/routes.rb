b8bff2d0404b72a0d2fc732907e07daa1abf633a
ahoward <ara.t.howard@gmail.com>
2018-04-12 11:56:16 -0600

    wip @ Thu Apr 12 11:56:16 MDT 2018

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  get '*_/media/*path', :format => false, :to => redirect('/media/%{path}')
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'site#index'
+
 end
